f21edd453f437b1cc8e5fcc79c88b1956441f690
adf0423 <adf0423@gmail.com>
2023-08-18 10:19:59 +0900

    jestがパスするように変更

--- frontend/__test__/Home.test.tsx
+++ frontend/__test__/Home.test.tsx
@@ -3,7 +3,9 @@
  */
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
+import 'jest-styled-components'
 import Home from '../src/app/page';
+import './globals.css';
 
 describe('Rendering', () => {
   it('Should render hello text', () => {
